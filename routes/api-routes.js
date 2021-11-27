aedc2e5a9559f314c2cd99745f38a00dde894c66
Justin Oeters <justin.oeters@gmail.com>
2021-11-27 12:21:09 -0500

    api routes laid out

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 // dependencies
 const express = require('express');
+const { route } = require('./html-routes');
 const router = express.Router();
 
 //get all workouts
@@ -36,7 +37,24 @@ router.put("workout/:id", (req, res) => {
     });
 });
 
-// get chart data
-
+// get chart data past 7 days weight and duration
+router.get("/workout/range", (req, res) => {
+    Workout.find({})
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+    .sort( { "day": -1, "_id": -1 })
+    .limit(7)
+    .then(dbWorkout => {
+        dbWorkout.reverse();
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
 
 module.exports = router;
