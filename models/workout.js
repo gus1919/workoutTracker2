f5ac3df5186d6849dcb763d761f634876c860600
Justin Oeters <justin.oeters@gmail.com>
2021-11-28 20:53:02 -0500

    added body parser

--- models/workout.js
+++ models/workout.js
@@ -3,17 +3,38 @@ const Schema = mongoose.Schema;
 
 // create workout schema
 const workoutSchema = new Schema({
-  day: {type: Date, default: () => Date.now()},
-  exercises: [{
-
-    type: {String, allowNull: false},
-    name: {String, allowNull: false},
-    duration: {Number, allowNull: false},
-    weight: {Number},
-    reps: {Number},
-    sets: {Number},
-    distance: {Number}    
-    }]
+  exercise: [
+    {
+      type: {
+        type: String,
+        required: 'Enter type of exercise to perform',
+      },
+      name: {
+        type: String,
+        required: 'Enter exercise name to perform',
+      },
+      duration: {
+        type: Number,
+        required: 'Enter duration in minutes',
+      },
+      distance: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+    },
+  ],
+  day: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
