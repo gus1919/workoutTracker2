0eb1bcc470a15b045d0cc0cdee9010919817656c
Justin Oeters <justin.oeters@gmail.com>
2021-11-28 20:58:07 -0500

    added body parser

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 
 
 const PORT = process.env.PORT || 3000;
 
 //middleware
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(logger("dev"));
 
 //connecting to mongo and making sure its working
 mongoose.connect(process.env.ATLAS_URI || "mongodb://localhost/workout", {
